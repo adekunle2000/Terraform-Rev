807b18e6c9f5b9c3f9b68f21f86d52a3e0964b2e
adekunle2000 <aolurin1@gmail.com>
2021-12-10 01:09:08 -0600

    I sent terraform.tsstate-file to s3

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Terraform-Rev
+# Mr terrence has i just want to create a tsstate.tf in s3.
--- provider.tf
+++ provider.tf
@@ -1,4 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.61.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
     region = "us-east-1"
     
+}
+
+terraform {
+  backend "s3" {
+    bucket = "amalachitta"
+    key    = "notepad/documents/money"
+    region = "us-east-1"
+  }
 }
